9e62ac0aeb80cec835767194f083f9d0397fbd62
skravche <sktehcno92@gmail.com>
2019-02-22 17:47:21 +0200

    commit need to change style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,6 +10,13 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Phone</h1>
+        <p>Search:</p>
+        <input type="input" name="search" />
+        <p>Sort by:</p>
+        <select>
+          <option>Alphabetical</option>
+          <option>Newest</option>
+        </select>
         <Route path="/" component={ProductListView} exact />
         <Route path="/:productView" component={ProductView} />
         <Route path="/productListView" component={ProductListView} />
